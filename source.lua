15505bdceb99a9bbaff2ac0860474a5ad94a2653
RZMVVZ <144466838+RZMVVZ@users.noreply.github.com>
2023-09-08 17:52:40 +0200

    Update source.lua

--- source.lua
+++ source.lua
@@ -2595,5 +2595,30 @@ loop = RunService.RenderStepped:Connect(function()
    end
 end)
 
+local Noclip = nil
+local Clip = nil
+
+function noclip()
+	Clip = false
+	local function Nocl()
+		if Clip == false and game.Players.LocalPlayer.Character ~= nil then
+			for _,v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+				if v:IsA('BasePart') and v.CanCollide and v.Name ~= floatName then
+					v.CanCollide = false
+				end
+			end
+		end
+		wait(0.21) -- basic optimization
+	end
+	Noclip = game:GetService('RunService').Stepped:Connect(Nocl)
+end
+
+function clip()
+	if Noclip then Noclip:Disconnect() end
+	Clip = true
+end
+
+noclip() -- to toggle noclip() and clip()
+
 task.delay(3.5, RayfieldLibrary.LoadConfiguration, RayfieldLibrary)
 return RayfieldLibrary
